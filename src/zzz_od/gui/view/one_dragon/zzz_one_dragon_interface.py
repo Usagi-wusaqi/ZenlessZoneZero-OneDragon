395d7d08f04d4477816f58ded4a1ff6cc8b339a0
Usagi-wusaqi <222152280+Usagi-wusaqi@users.noreply.github.com>
2026-03-12 13:09:43 +0800

    refactor: 预备编队识别从游戏助手移到一条龙

--- src/zzz_od/gui/view/one_dragon/zzz_one_dragon_interface.py
+++ src/zzz_od/gui/view/one_dragon/zzz_one_dragon_interface.py
@@ -4,6 +4,7 @@ from one_dragon_qt.widgets.pivot_navi_interface import PivotNavigatorInterface
 from one_dragon_qt.widgets.setting_card.app_run_card import AppRunCard
 from zzz_od.context.zzz_context import ZContext
 from zzz_od.gui.view.one_dragon.mouse_sensitivity_checker_interface import MouseSensitivityCheckerInterface
+from zzz_od.gui.view.one_dragon.predefined_team_checker_interface import PredefinedTeamCheckerInterface
 from zzz_od.gui.view.one_dragon.setting_team_interface import SettingTeamInterface
 from zzz_od.gui.view.one_dragon.zzz_one_dragon_run_interface import ZOneDragonRunInterface
 
@@ -26,6 +27,7 @@ class ZOneDragonInterface(PivotNavigatorInterface):
         self.add_sub_interface(ZOneDragonRunInterface(self.ctx))
         self.add_sub_interface(SettingTeamInterface(self.ctx))
         self.add_sub_interface(MouseSensitivityCheckerInterface(self.ctx))
+        self.add_sub_interface(PredefinedTeamCheckerInterface(self.ctx))
 
     def on_interface_shown(self):
         super().on_interface_shown()
