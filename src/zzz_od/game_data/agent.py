d847fab9d49c7692f027d1907fcd9535ad6ba013
ShadowLemoon <119576779+ShadowLemoon@users.noreply.github.com>
2026-05-10 15:07:57 +0800

    chore: 普罗米亚 -> 普罗米娅

--- src/zzz_od/game_data/agent.py
+++ src/zzz_od/game_data/agent.py
@@ -507,6 +507,6 @@ class AgentEnum(Enum):
     CISSIA = Agent('cissia', '希希芙', RareTypeEnum.S, AgentTypeEnum.ATTACK, DmgTypeEnum.ELECTRIC, ['cissia'],
                    state_list=[])
 
-    PROMEIA = Agent('promeia', '普罗米亚', RareTypeEnum.S, AgentTypeEnum.ANOMALY, DmgTypeEnum.ICE, ['promeia'],
-                    state_list=[AgentStateDef('普罗米亚-霜刑', AgentStateCheckWay.COLOR_RANGE_CONNECT, 'promeia_ss',
+    PROMEIA = Agent('promeia', '普罗米娅', RareTypeEnum.S, AgentTypeEnum.ANOMALY, DmgTypeEnum.ICE, ['promeia'],
+                    state_list=[AgentStateDef('普罗米娅-霜刑', AgentStateCheckWay.COLOR_RANGE_CONNECT, 'promeia_ss',
                                               hsv_color=(0,255,255), hsv_color_diff=(90, 255, 50), connect_cnt=2)])
