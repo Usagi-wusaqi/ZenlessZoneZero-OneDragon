656578872bf048a982d200f3284da6a58f66c3ee
Usagi-wusaqi <222152280+Usagi-wusaqi@users.noreply.github.com>
2026-07-19 16:35:23 +0800

    fix: 固定 ppocrv6 模型标识

--- src/one_dragon/base/matcher/ocr/onnx_ocr_matcher.py
+++ src/one_dragon/base/matcher/ocr/onnx_ocr_matcher.py
@@ -18,7 +18,7 @@ from one_dragon.utils.i18_utils import gt
 from one_dragon.utils.log_utils import log
 
 DEFAULT_OCR_MODEL_NAME: str = 'ppocrv6'
-PPOCRV6_MODEL_NAME: str = DEFAULT_OCR_MODEL_NAME
+PPOCRV6_MODEL_NAME: str = 'ppocrv6'
 GITHUB_DOWNLOAD_URL: str = 'https://github.com/OneDragon-Anything/OneDragon-Env/releases/download'
 GITEE_DOWNLOAD_URL: str = 'https://gitee.com/OneDragon-Anything/OneDragon-Env/releases/download'
 
